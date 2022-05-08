9488ad9cde374c8236d9511670449df0c7bfca73
Valentin Karrica <vale26@hotmail.gr>
2022-05-08 11:13:31 +0100

    heroku first commit

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port  = process.env.PORT || 8000
 
 // Setting EJS as templating engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.listen(8000, () => {
-    console.log("APP IS LISTENING ON PORT 8000!")
+app.listen(port, () => {
+    console.log(`APP IS LISTENING ON PORT ${port}!`)
 })
-// app.get('/', (req, res) => {
-//     res.render('homepage', { name: 'Monroe' })
-// })
 app.use(express.static(__dirname + '/public'));
 
 
